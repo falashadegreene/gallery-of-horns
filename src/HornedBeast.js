0104cacc916032f2f70191f8f1d5bec5fcc30da8
Falashade Greene <falashadegreene@gmail.com>
2022-06-13 23:49:44 -0400

    created components and completed featured tasks

---
+++ src/HornedBeast.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render () {
+    return (
+     <article>
+       <h2>{this.props.title}</h2>
+       <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="narwhal" title="UniWhal">
+       </img>
+       <p>A unicorn and a narwhal nuzzling their horns</p>
+     </article>
+    );
+  }
+}
+
+export default HornedBeast;
