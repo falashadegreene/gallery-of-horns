99250864fe7cb7ff99ad53a0e4a1a2224d5a7450
Falashade Greene <falashadegreene@gmail.com>
2022-06-16 02:10:42 -0400

    added Modal to App file

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import data from './data.json';
 import './Main.css';
 
 
 class Main extends React.Component {
  render () {
-  let horns = data.map((horns,idx) => (
+  let horns = this.props.data.map((horns,idx) => (
     <HornedBeast 
       title = {horns.title} 
       imageUrl = {horns.image_url} 
       description={horns.description}
       key={idx}
+      handleOnShowModal={this.props.handleOnShowModal}
     />
     ));
 
