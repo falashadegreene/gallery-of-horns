a95636a3bc59b10d4d9b7ec6f4b4588397a310e8
Falashade Greene <falashadegreene@gmail.com>
2022-06-17 23:30:24 -0400

    added form to main js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './Main.css';
+
 
 
 class Main extends React.Component {
@@ -10,16 +13,27 @@ class Main extends React.Component {
       title = {horns.title} 
       imageUrl = {horns.image_url} 
       description={horns.description}
-      key={idx}
+      key={horns._id}
       horns={horns}
       handleOnShowModal={this.props.handleOnShowModal}
     />
     ));
 
     return (
-
-      
       <main>
+    <Form onSubmit={this.handleSubmit}>
+      <Form.Group>
+        <Form.Select onChange={this.handleChange}>
+            <option value="all">all</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="100">100</option>     
+        </Form.Select>
+      </Form.Group>
+      <Button type="submit">submit</Button>
+    </Form>
+      
       {horns}
       </main>
       
